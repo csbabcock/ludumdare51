3d6605b21eabaa34ad8541d9d6feb37bb0c08d80
csbabcock <christianbabcock@knights.ucf.edu>
2022-10-01 21:48:13 -0400

    Added flickering light with sound

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random=UnityEngine.Random;
+
+public class LightFlicker : MonoBehaviour
+{
+    public Light light;
+
+    public AudioSource lightSound;
+
+    public float minTime;
+
+    public float maxTime;
+
+    public float flickerTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flickerTimer = Random.Range(minTime, maxTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LightsFlickering();
+    }
+
+    void LightsFlickering()
+    {
+        if (flickerTimer > 0 )
+        {
+            flickerTimer -= Time.deltaTime;
+        }
+
+        if (flickerTimer <= 0)
+        {
+            light.enabled = !light.enabled;
+            flickerTimer = Random.Range(minTime, maxTime);
+            lightSound.Play();
+        }
+    }
+}
